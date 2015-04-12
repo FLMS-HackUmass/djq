8c30d39cc4446a9640994d2b9d6dad471a9033cd
Gabe <gmarkari@umass.edu>
2015-04-11 21:04:50 -0400

    Search and result with youtube api

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+//ROUTES
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var addsearch = require('./routes/addsearch');
 var app = express();
 
 // view engine setup
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
+app.use('/add', addsearch);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
--- public/javascripts/youtube.js
+++ public/javascripts/youtube.js
@@ -1,31 +1,52 @@
+$(document).ready(function(){
+  var input = $('#searchbar');
+  input.keyup(function(){
+     search(input.val());
+  });
+
+});
+
+
 function showResponse(response){
-  console.log("showresponse");
-  var res = JSON.stringify(res, '', 2);
-  document.getElementById('text').innerHTML += res;
+  $('#results').empty();
+  var res = response.items;
+  console.log(res[0]);
+  res.forEach(function(video){
+    var djObj ={};
+    djObj.title    = video.snippet.title;
+    djObj.url      = video.id.videoId;
+    djObj.thumbnail = video.snippet.thumbnails.default.url; 
+    $('#results').append("<li>"+djObj.title+
+           "<a href=https://www.youtube.com/watch?v="
+              +djObj.url+"><img src="+djObj.thumbnail+"></a>"
+                         +"</li>");
+  });
+
 }
 
 function onClientLoad(){
-  console.log("onclientload");
+
   gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
 }
 
 function onYouTubeApiLoad(){
-  console.log("onapiload");
+
   gapi.client.setApiKey('AIzaSyCGYJ3Qyz27hn0MKT7CSHlf7l9kB-qkLgY');
-  search();
+//  search();
 }
 
-function search(){
-  console.log("search");
+function search(query){
+
   var req = gapi.client.youtube.search.list({
-    part: 'snippet'
+    part: 'snippet',
+    q: query
   });
 
   req.execute(onSearchResponse);
 }
 
 function onSearchResponse(response) {
-  console.log("onsearchresponse");
-  console.log(response);
+ 
+//  console.log(response);
   showResponse(response);
 }
