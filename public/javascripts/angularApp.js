e5ddafbe57283a4855e01aa7fa37f36e4cd0f163
NateFuller <fatenuller@gmail.com>
2015-04-14 04:51:04 -0400

    Hiding/showing is now depicted by a slide in/out animation

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -161,17 +161,19 @@ app.controller('UsersCtrl', [
 			queue.getAll($scope.username);
 		}
 
-		$scope.hidePlayer = function() {
-			// hide the player
-			$('#player-container').toggleClass('hidden');
-
+		$scope.slidePlayer = function() {
 			// change the button appearance
 			if ($('#player-container').hasClass('hidden')) {
-				$('#hide-player').text("Show Player ");
+				$('#player-container').removeClass('hidden');
+				$('#player-container').slideDown();
+				$('#hide-player').text("Hide Player ");
 				$('#hide-player').append("<span class='glyphicon glyphicon-eye-open'></span>");
 			}
 			else {
-				$('#hide-player').text("Hide Player ");
+				$('#player-container').slideUp(function() {
+					$('#player-container').addClass('hidden');
+				});
+				$('#hide-player').text("Show Player ");
 				$('#hide-player').append("<span class='glyphicon glyphicon-eye-close'></span>");
 			}
 		}
